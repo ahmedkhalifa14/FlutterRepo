7c0f1feec966b99a70874a8e91d4df141dcec719
Ahmed Khalifa <ahmedkhalifaserag@gmail.com>
2025-08-02 19:45:17 +0300

    business card app

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,50 +3,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(BusniessCardApp());
+}
+
+class BusniessCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-          backgroundColor: Colors.orange,
-        ),
+        backgroundColor: Color(0xff2B475E),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(height: 150, width: 200, color: Colors.green),
-            Container(height: 150, width: 250, color: Colors.yellow),
-            Container(height: 150, width: 150, color: Colors.red),
+            CircleAvatar(
+              radius: 112,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 110,
+                backgroundImage: AssetImage("images/personal_img.jpg"),
+              ),
+            ),
+            Text(
+              "Ahmed Khalifa",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Color(0xff6C8090),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              color: Color(0xff6c8090),
+              indent: 40,
+              endIndent: 40,
+              thickness: 2,
+            ),
+
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                leading: Icon(Icons.phone, size: 32, color: Color(0xff2B475E)),
+                title: Text("(+20) 1040691764", style: TextStyle(fontSize: 24)),
+              ),
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                leading: Icon(Icons.mail, size: 32, color: Color(0xff2B475E)),
+                title: Text(
+                  "ahmed123@gmail.com",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
           ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
-// Container(
-//         color: Colors.blue,
-//         height: 100,
-//         width: 200,
-//         alignment: Alignment.center,
-//         // margin: EdgeInsets.only(
-//         //   left: 30,
-//         //   right: 30,
-//         //   top: 300,
-//         //   bottom: 300
-//         // ),
-//         margin:EdgeInsets.symmetric(
-//         horizontal: 30,
-//         vertical: 300
-//         ),
-//         child: Padding(
-//           padding: EdgeInsets.all(30),
-//           child: Text("Hello World!",
-//           style: TextStyle(
-//           fontSize: 40,
-//           color: Colors.blue,
-//           fontWeight: FontWeight.w900,
-//         //backgroundColor: Colors.black
-
-//         ),
-//         ),)
-//         )
